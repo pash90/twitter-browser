4ef3d318a168bf207f612415cb68ccc047cc976a
Parth Mehta <tech@iamdifferent.io>
2018-11-15 13:35:40 +1100

    :art: restructuring

---
+++ src/App/index.test.tsx
@@ -0,0 +1,12 @@
+/** Libraries */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/** Components */
+import App from './';
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
